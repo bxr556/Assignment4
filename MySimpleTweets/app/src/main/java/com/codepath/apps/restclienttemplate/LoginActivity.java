9757b0677b7a3edd7ed42ff3efebdb832e9a8e88
pan maker <pan.maker.1870@gmail.com>
2017-10-06 21:10:36 -0700

    Added optional Robust error handling

--- MySimpleTweets/app/src/main/java/com/codepath/apps/restclienttemplate/LoginActivity.java
+++ MySimpleTweets/app/src/main/java/com/codepath/apps/restclienttemplate/LoginActivity.java
@@ -1,12 +1,17 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Toast;
 
 import com.codepath.oauth.OAuthLoginActionBarActivity;
+
+import java.io.IOException;
 
 public class LoginActivity extends OAuthLoginActionBarActivity<TwitterClient> {
 
@@ -39,6 +44,15 @@ public class LoginActivity extends OAuthLoginActionBarActivity<TwitterClient> {
 	@Override
 	public void onLoginFailure(Exception e) {
 		e.printStackTrace();
+
+		if (!isNetworkAvailable()){
+			Toast.makeText(this, "Network is not available, please retry later", Toast.LENGTH_SHORT).show();
+		}
+
+		if (!isOnline()){
+			Toast.makeText(this,"Your device is not online, please retry later",Toast.LENGTH_LONG).show();
+		}
+
 	}
 
 	// Click handler method for the button used to start OAuth flow
@@ -46,6 +60,25 @@ public class LoginActivity extends OAuthLoginActionBarActivity<TwitterClient> {
 	// This should be tied to a button used to login
 	public void loginToRest(View view) {
 		getClient().connect();
+	}
+
+
+	private Boolean isNetworkAvailable(){
+		ConnectivityManager connectivityManager
+				= (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+		return activeNetworkInfo!=null && activeNetworkInfo.isConnectedOrConnecting();
+	}
+
+	public boolean isOnline(){
+		Runtime runtime =Runtime.getRuntime();
+		try{
+			Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
+			int exitValue = ipProcess.waitFor();
+			return (exitValue==0);
+		}catch(IOException e){e.printStackTrace();}
+		catch(InterruptedException e){e.printStackTrace();};
+			return false;
 	}
 
 }
